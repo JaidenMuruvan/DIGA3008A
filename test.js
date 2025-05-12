0bf7e948cb0841d1bd237cf2e6c22a412358b789
jaidenmuruvan <2726725@students.wits.ac.za>
2025-05-12 22:10:15 +0200

    More css changes and added blogs cards

--- test.js
+++ test.js
@@ -6,3 +6,44 @@ console.log(car.model);
 randomNumber = Math.random();
 console.log(randomNumber);
 
+function Exercise1(){
+    let width = 5;
+    let length = 6;
+
+    let area = width * length;
+}
+
+function Exercise2(){
+    let x = 3;
+    let y = 3;
+    let sum = x + y;
+
+    if(x == y){
+        return sum*3;
+    }
+    else{
+        return sum
+    }
+    return (x == y) ? sum*3 : sum;
+}
+
+function Exercise3(){
+    let randomNum1 = Math.floor(Math.random() * 51);
+    let randomNum2 = Mathfloor(Math.random() * 51);
+
+    if(randomNum1 == 50 || (randomNum1+randomNum2 ) === 50){
+        return true;
+    }
+    
+    else{
+        return false;
+    }
+}
+
+function Exercise4(voltage, current){
+    return current * voltage;
+}
+
+console.log(Exercise4);
+
+
