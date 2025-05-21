f5cb599857416bdb092a49e99c61d59d26951fee
jaidenmuruvan <2726725@students.wits.ac.za>
2025-05-22 00:24:50 +0200

    Back to top button added

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if(pos>100){
+        scrollProgress.style.display = "grid";
+    } else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    })
+    scrollProgress.style.background = `conic-gradient(#48cfcb ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
 //DOM Object
-window.alert("Hello there, welcome!");
+// window.alert("Hello there, welcome!");
