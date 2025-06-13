2ece82242525b25f5e5e1c1fd226f9bf14cbff0c
jaidenmuruvan <2726725@students.wits.ac.za>
2025-06-13 21:59:40 +0200

    API is fully functional

--- Portfolio/portfolioapi.js
+++ Portfolio/portfolioapi.js
@@ -21,13 +21,26 @@ async function tvMazeApi(query){
 }
 
 function makeImages(movies){
+    container.innerHTML = ''; //Clears the previous results
+
     for(let movie of movies){
-        let src = movie.show.image.medium;
+        if (!movie.show.image) continue; //Skips search if theres no image
 
+        const src = movie.show.image.medium;
+        const url = movie.show.url; //Gets the TVmaze show URL
+
+        //Created anchor element
+        const link = document.createElement('a');
+        link.href = url;
+        link.target = '_blank'; //Opens a new tab
+
+        //Created image element
         const img = document.createElement('img');
-        img.src=src;
+        img.src = src;
+        img.alt = movie.show.name;
 
-        container.appendChild(img);
-
+        //Appended image to link, then linked to container
+        link.appendChild(img);
+        container.appendChild(link);
     }
 }
